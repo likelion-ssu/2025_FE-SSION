0d72bebc772883e687e6c589d9f4921e5b0f4970
sunwoo-h <flexible8011@gmail.com>
2025-06-18 20:37:16 +0900

    refact: 타이틀 부모 라우터로 이동

--- week03/sunwoo/src/App.jsx
+++ week03/sunwoo/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  margin: 80px 150px;
+`;
 
 function App() {
   return (
-    <div>
+    <MainContainer>
+      <h2 style={{ textAlign: "center", marginBottom: "40px" }}>
+        황선우의 미니 블로그
+      </h2>
       <Outlet />
-    </div>
+    </MainContainer>
   );
 }
 
--- week03/sunwoo/src/component/page/MainPage.jsx
+++ week03/sunwoo/src/component/page/MainPage.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-import styled from "styled-components";
 import PostList from "../list/PostList";
 import Button from "../ui/Button";
 import TextInput from "../ui/TextInput";
 import CommentList from "../list/CommentList";
 
-const MainContainer = styled.div`
-  margin: 80px 150px;
-`;
-
 const MainPage = () => {
   return (
-    <MainContainer>
-      <h3 style={{ textAlign: "center" }}>블로그</h3>
-      <div style={{ height: "50px" }} />
+    <>
       <Button title="글 작성하기"></Button>
       <TextInput placeholder="글을 작성해 주세요.." />
       <PostList />
       <CommentList />
-    </MainContainer>
+    </>
   );
 };
 
