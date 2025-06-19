da2fb7420fc0838433fbde862adff24ed6aa4ede
sunwoo-h <flexible8011@gmail.com>
2025-06-19 15:11:50 +0900

    feat: 게시글 작성 페이지 뒤로가기 버튼 구현

--- week03/sunwoo/src/component/page/PostWritePage.jsx
+++ week03/sunwoo/src/component/page/PostWritePage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 const PostWritePage = () => {
+  const nav = useNavigate();
+  const moveBack = () => {
+    nav("/");
+  };
+
   return (
     <div>
-      <TextInput placeholder={"제목을 입력하세요"} />
+      <Button title={"뒤로가기"} onClick={moveBack} />
+      <div style={{ height: "10px" }} />
+      <TextInput height={38} placeholder={"제목을 입력하세요"} />
       <TextInput height={500} placeholder={"내용을 입력하세요"} />
     </div>
   );
