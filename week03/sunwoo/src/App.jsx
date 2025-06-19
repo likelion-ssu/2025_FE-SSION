07c188867a2f86894d1459ef06c243ed9fed0b59
sunwoo-h <flexible8011@gmail.com>
2025-06-19 16:09:51 +0900

    refact: App컴포넌트 수정을 통해 화면 레이아웃 변경

--- week03/sunwoo/src/App.jsx
+++ week03/sunwoo/src/App.jsx
@@ -3,16 +3,26 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 const MainContainer = styled.div`
-  margin: 80px 150px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
 `;
 
 function App() {
   return (
     <MainContainer>
-      <h2 style={{ textAlign: "center", marginBottom: "40px" }}>
-        황선우의 미니 블로그
-      </h2>
-      <Outlet />
+      <Container>
+        <h2 style={{ textAlign: "center", marginBottom: "40px" }}>
+          황선우의 미니 블로그
+        </h2>
+        <Outlet />
+      </Container>
     </MainContainer>
   );
 }
