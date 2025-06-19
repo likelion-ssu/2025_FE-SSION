e85818a9fd932aecbd1a6d7a8e6f7e1b24f0ea76
sunwoo-h <flexible8011@gmail.com>
2025-06-19 15:40:02 +0900

    feat: 메인페이지 게시글 검색 기능 구현

--- week03/sunwoo/src/component/page/MainPage.jsx
+++ week03/sunwoo/src/component/page/MainPage.jsx
@@ -4,6 +4,7 @@ import Button from "../ui/Button";
 import TextInput from "../ui/TextInput";
 import { useNavigate } from "react-router-dom";
 import data from "../../../data.json";
+import { useState } from "react";
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -13,10 +14,22 @@ const MainPage = () => {
 
   const posts = data;
 
+  const [searchPost, setSearchPost] = useState("");
+
+  const filterdPost = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchPost.toLowerCase())
+  );
+
   return (
     <>
+      <TextInput
+        height={38}
+        onChange={(e) => setSearchPost(e.target.value)}
+        placeholder={"검색어를 입력하세요.."}
+      />
+      <div style={{ marginBottom: "4px" }} />
       <Button title="글 작성하기" onClick={moveWritePage} />
-      <PostList posts={posts} />
+      <PostList posts={filterdPost} />
     </>
   );
 };
