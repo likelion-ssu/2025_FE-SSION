d4689591ae9fe909f5c379cc559a5c058a107579
sunwoo-h <flexible8011@gmail.com>
2025-06-19 14:34:08 +0900

    remove: 메인페이지 불필요한 요소 제거

--- week03/sunwoo/src/component/page/MainPage.jsx
+++ week03/sunwoo/src/component/page/MainPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PostList from "../list/PostList";
 import Button from "../ui/Button";
 import TextInput from "../ui/TextInput";
-import CommentList from "../list/CommentList";
 import { useNavigate } from "react-router-dom";
 import data from "../../../data.json";
 
@@ -17,9 +16,7 @@ const MainPage = () => {
   return (
     <>
       <Button title="글 작성하기" onClick={moveWritePage} />
-      <TextInput placeholder="글을 작성해 주세요.." />
       <PostList posts={posts} />
-      <CommentList />
     </>
   );
 };
