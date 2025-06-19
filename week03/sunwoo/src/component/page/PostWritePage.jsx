af166b623fb30bd912d16ce30463f81cf59b3d1b
sunwoo-h <flexible8011@gmail.com>
2025-06-19 15:08:44 +0900

    feat: 게시글 작성 페이지 제목입력, 내용입력 TextInput 구현

--- week03/sunwoo/src/component/page/PostWritePage.jsx
+++ week03/sunwoo/src/component/page/PostWritePage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import TextInput from "../ui/TextInput";
 
 const PostWritePage = () => {
-  return <div>포스트 작성 페이지입니다.</div>;
+  return (
+    <div>
+      <TextInput placeholder={"제목을 입력하세요"} />
+      <TextInput height={500} placeholder={"내용을 입력하세요"} />
+    </div>
+  );
 };
 
 export default PostWritePage;
