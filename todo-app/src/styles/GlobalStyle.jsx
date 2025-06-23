c5bed230e6d92bc9275d0a90b207a2d1165e4b2e
ukyeong-user <hyk040520@naver.com>
2025-06-24 01:57:30 +0900

    chore : 폰트 적용 및 body 설정

---
+++ todo-app/src/styles/GlobalStyle.jsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Ownglyph_ParkDaHyun';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/2411-3@1.0/Ownglyph_ParkDaHyun.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  /* 부모 요소가 높이가 있어 의미 있다는 의미 */
+  html, body, #root {
+    height: 100%;     
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    line-height: 0;
+    font-family: 'Ownglyph_ParkDaHyun', sans-serif;
+  }
+
+  button {
+    font-family: 'Ownglyph_ParkDaHyun', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
